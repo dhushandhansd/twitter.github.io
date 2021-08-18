eb4c8c11ae8813ef500ff89f4b3ce0cbda5daed9
Shreedhushandhan Umasankarkumudhini <shreedhushandhan.umasankarkumudhini@cesltd.com>
2021-08-18 21:03:07 +0530

    Login CSS fixed, Login Page path bug resolved

--- assets/script/login.js
+++ assets/script/login.js
@@ -2,7 +2,7 @@ const loginButton = document.getElementById('login-nav')
 const userInput = document.querySelector('.email-address')
 const passwordInput = document.querySelector('.password')
 
-const loginNav = document.getElementById('login-nav-btn')
+const navLogin = document.getElementById('nav-login')
 
 
 
@@ -23,12 +23,12 @@ loginButton.addEventListener('click', () => {
 function loginActivator() {
     if(userInput.value != '' || userInput.value != null) {
         if(passwordInput.value != '' || passwordInput.value != null) {
-            loginBtn.classList.remove('inactive')
+            loginButton.classList.remove('inactive')
             document.cookie = 'auth=false'
         }
     }
 }
 
-loginNav.addEventListener('click', () => {
+navLogin.addEventListener('click', () => {
     location.href = 'https://dhushandhansd.github.io/twitter.github.io/login'
 })
